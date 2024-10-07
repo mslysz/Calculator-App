494963c2acac23b894eb6af4e3ae6152808520de
mslysz <macslysz@gmail.com>
2024-10-08 00:20:07 +0100

    I have created a caclFunc function to perform calculator calculations

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.result');
 let calculation = '';
+
+const calcFunc = function () {
+  if (this.id === '=') {
+    let result = eval(calculation);
+    calculation = result;
+  } else {
+    calculation += this.id;
+  }
+  console.log(calculation);
+  result.textContent = calculation;
+};
+
+buttons.forEach((button) => {
+  button.addEventListener('click', calcFunc);
+});
